4bd236c9172036f886dc56deb154ccbaed9755d7
Ahmed Yousef <ahmedyousefx1@gmail.com>
2020-05-13 15:36:07 +0200

    Adding logger middleware for reqests and responses

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 81;
 const app = express();
 
 //Injecting Middlewares
@@ -10,4 +9,4 @@ app.use(bodyParser.json());
 
 app.use('/todoApp/api', require('./routes/api/todos'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const uuid = require('uuid');
-const router = express.Router();
 const todos = require('../../Todos');
+const logger = require('../../middlewares/logger');
+
+const router = express.Router();
+
+const logReqAndRes = (req, res) => {
+    logger.info('/ Request', { request: req.get('host') + req.originalUrl });
+    logger.info('/ Response', res);
+};
 
 // Get All Tasks
-router.get('/todos', (req, res) => res.json(todos));
-router.get('/health', (req, res) => res.json(todos));
+router.get('/todos', (req, res) => {
+    res.json(todos);
+    logReqAndRes(req, todos);
+});
+router.get('/health', (req, res) => {
+    res.json(todos);
+    logReqAndRes(req, todos);
+});
 
 // Get Single Task
 router.get('/todos/:id', (req, res) => {
     const found = todos.some(todo => todo.id === req.params.id);
 
     if (found) {
-        res.json(todos.filter(todo => todo.id === req.params.id));
+        let filteredTodos = todos.filter(todo => todo.id === req.params.id);
+        res.json(filteredTodos);
+        logReqAndRes(req, filteredTodos);
     } else {
         res.status(400).json({ msg: `No todo with the id of ${req.params.id}` });
     }
@@ -31,6 +46,7 @@ router.post('/todos', (req, res) => {
 
     todos.push(newTodo);
     res.json(todos);
+    logReqAndRes(req, todos);
 });
 
 // Update Task
@@ -43,6 +59,7 @@ router.patch('/todos/:id', (req, res) => {
             if (todo.id === req.params.id) {
                 todo.title = updatedTodo.title ? updatedTodo.title : todo.title;
                 res.json({ msg: 'Todo updated', todo });
+                logReqAndRes(req, todo);
             }
         });
     } else {
@@ -56,7 +73,9 @@ router.delete('/todos/:id', (req, res) => {
 
     if (found) {
         let removeIndex = todos.map(todo => todo.id).indexOf(req.params.id);
-        res.json({ msg: 'Todo deleted', todos: todos.splice(removeIndex, 1) });
+        let removedTodo = todos.splice(removeIndex, 1);
+        res.json({ msg: 'Todo deleted', todo: removedTodo });
+        logReqAndRes(req, removedTodo);
     } else {
         res.status(400).json({ msg: `No todo with the id of ${req.params.id}` });
     }
